3e4fa246052cae99109fa6bcbf7a03960b2fde35
LinHanLi <h1nlee@163.com>
2022-09-02 15:15:22 +0800

    Update eval.py

--- yaya_quant/re/eval.py
+++ yaya_quant/re/eval.py
@@ -4,12 +4,12 @@ import numpy as np
 
 
 #sharp ratio and annualized return
-def sharpe(net, bench, start_date, end_date):
+def sharpe(net, bench):
     change = np.array([(net[i] - net[i-1])/net[i-1] for i in range(len(net))])
     sigma = change.std()*np.sqrt(250)
-    dur = (end_date - start_date).days
+    dur = len(net)
     r =  net[-1]/net[0]
-    Ep = r**(365/dur) -1
+    Ep = r**(250/dur) -1
     return (Ep - bench)/sigma, sigma, Ep
 
 # max drawdown  input is a time series
