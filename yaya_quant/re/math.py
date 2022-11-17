008c08f21ad4d35c54696634944eba95cbd6d855
LinHanLi <h1nlee@163.com>
2022-11-17 17:25:53 +0800

    Update math.py

--- yaya_quant/re/math.py
+++ yaya_quant/re/math.py
@@ -2,7 +2,7 @@ import numpy as np
 import copy
 
 
-# convert continue variable to discrete
+# convert continue variable to discrete and count
 def scatter_continue(_a,lena):
     a = copy.copy(_a)
     a.sort()
@@ -18,3 +18,21 @@ def scatter_continue(_a,lena):
             a_count[j] += 1
             a_cut += a_d
     return a_count
+
+# label = 0, 1, 2, ...
+def class_continue(_a,lena):
+    a_max = a.max()
+    a_min = a.min()
+    new_a = -np.ones(len(a))
+    linspace = np.linspace(a_min,a_max,n+1)
+    
+    for i in range(len(a)):
+    diff = a[i]-linspace
+    for j in range(len(diff)):
+        if diff[j] >= 0:
+            continue
+        else:
+            new_a[i]=j-1
+            break
+            
+    return new_a
