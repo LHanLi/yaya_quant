8723d858f77cb11a21bdedc8a82ac426a4202c61
LinHanLi <h1nlee@163.com>
2022-12-12 18:43:44 +0800

    Create plot.py

---
+++ yaya_quant/re/plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
+
+
+del matplot(r,c):
+  #plot
+  #run configuration 
+  plt.rcParams['font.size']=8
+  #plt.rcParams['font.family'] = 'Arial'
+  plt.rcParams['axes.linewidth']=0.5
+  plt.rcParams['axes.grid']=True
+  plt.rcParams['grid.linestyle']='--'
+  plt.rcParams['grid.linewidth']=0.2
+  plt.rcParams["savefig.transparent"]='True'
+  plt.rcParams['lines.linewidth']=0.8
+  plt.rcParams['lines.markersize'] = 1
+    
+  #subplot
+  fig,ax = plt.subplots(r,c)
+  plt.subplots_adjust(left=None, bottom=None, right=None, top=None, hspace = None, wspace=0.5)
+  
+  return fig,ax
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
