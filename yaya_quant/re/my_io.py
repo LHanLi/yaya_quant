efffa322ff2bc5b068f828f899f7b5f97fde4d54
LinHanLi <h1nlee@163.com>
2023-11-12 17:16:18 +0800

    my_io
    添加serial选项

--- yaya_quant/re/my_io.py
+++ yaya_quant/re/my_io.py
@@ -1,12 +1,18 @@
+import dill
+import pickle
 
 
-def save_pkl(a,filename):
-    import pickle
+# 反序列化一个对象
+
+def save_pkl(obj,filename,serial=False):
+    if serial:
+        obj = dill.dumps(obj)
     with open(filename,'wb') as file:
-        pickle.dump(a,file)
+        pickle.dump(obj,file)
 
-def read_pkl(filename):
-    import pickle
+def read_pkl(filename,serial=False):
     with open(filename,'rb') as file:
-        a = pickle.load(file)
-        return a
+        obj = pickle.load(file)
+        if serial:
+            obj = dill.loads(obj)
+        return obj
